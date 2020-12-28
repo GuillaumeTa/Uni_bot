d2446d84dbef362b80cac407ccb41f662f91a284
Alucard <guillaume.tatuszka@gmail.com>
2020-12-28 15:35:15 +0100

    Ajout des commandes !ping !server et !avatar

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 const Discord = require('discord.js');
+
+const {prefix, token} = require('./config.json');
+
+// eslint-disable-next-line one-var
+const client = new Discord.Client();
+
+client.login(token);
+
+client.once(
+    'ready',
+    () => {
+
+        // eslint-disable-next-line no-console
+        console.log('Ready!');
+
+    }
+);
+
+client.on(
+    'message',
+    (message) => {
+
+        if (message.content.startsWith(`${prefix}ping`)) {
+
+            const ping = `${Date.now() - message.createdTimestamp} ms`;
+
+            message.channel.send(`Le ping est ${ping}`);
+
+        } else if (message.content.startsWith(`${prefix}server`)) {
+
+            message.channel.send(`Le nom du serveur est : 
+            ${message.guild.name}\n
+            Nombre d'utilisateurs : 
+            ${message.guild.memberCount}`);
+
+        } else if (message.content.startsWith(`${prefix}avatar`)) {
+
+            if (!message.mentions.users.size) {
+
+                return message.channel.send(`Votre avatar est : 
+                ${message.author.displayAvatarURL({'format': 'gif'})}`);
+
+            }
+
+        }
+
+    }
+);
